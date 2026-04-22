b2442d9d19cb76524b94307dd3f8982cb8495993
SebastianSalling <sebastian-benjamin-s@hotmail.com>
2026-04-22 13:43:28 +0200

    Navbar update

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -5,12 +5,21 @@
 <template>
 
     <nav>
-        <button id="burgermenu"><img src="../assets/icons/burgermenu.png" alt="Burgermenu"></img></button>
+        <div>
+            <button id="burgermenu"><img src="../assets/icons/burgermenu.png" alt="Burgermenu"></img></button>
             
-        <router-link to="/"><img src="../assets/icons/PARTNER_LOGO.svg" alt="Scangrip partner site logo"></router-link>
-
-        <input type="text">
+            <router-link to="/"><img class="partner-logo" src="../assets/icons/PARTNER_LOGO.svg" alt="Scangrip partner site logo"></router-link>
+        </div>
+        
+        <div>
+            <input type="text">
+            <img class="search-icon" src="../assets/icons/search.png" alt="">
+        </div>
+        
     </nav>
+
+    <br>
+    <br>
 
     <h1>dette er en h1</h1>
     <h2>dette er en h2</h2>
@@ -30,6 +39,15 @@
             width: 50px;
             height: 50px;
         }
+    }
+
+    .partner-logo {
+        height: 50px;
+    }
+
+    .search-icon {
+            width: 30px;
+            height: 30px;
     }
 
 </style>
