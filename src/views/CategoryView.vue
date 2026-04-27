ec65a3483a48ffef5317b8077b81dac473013b20
Vincent Wiederhold <vincent.taliesin@gmail.com>
2026-04-27 12:59:52 +0200

    slettet script der ikke bruges

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -1,10 +1,8 @@
 <script setup>
 import Footer from '@/components/footer.vue';
-import Header from '@/components/Header.vue';
 import Navbar from '@/components/Navbar.vue';
 </script>
 <template>
-    <Header/>
     <Navbar/>
     <Footer/>
 </template>
