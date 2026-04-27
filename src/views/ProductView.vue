f91c75dda62ca83eb66bab73e36999f2809efe86
Vincent Wiederhold <vincent.taliesin@gmail.com>
2026-04-27 12:37:58 +0200

    ændring af navbar navngivning

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import Footer from '@/components/footer.vue';
 import Header from '@/components/Header.vue';
-import Navbar from '@/components/navbar.vue';
+import Navbar from '@/components/Navbar.vue';
 </script>
 <template>
     <Header/>
