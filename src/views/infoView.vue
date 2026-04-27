fc11a53c7e67eeb49c7f1288e76815973ebb4d0e
Vincent Wiederhold <vincent.taliesin@gmail.com>
2026-04-27 14:00:54 +0200

    infosite.ikoner

--- src/views/infoView.vue
+++ src/views/infoView.vue
@@ -7,22 +7,28 @@ import LargeIndexCard from '@/components/LargeIndexCard.vue';
     <Navbar/>
     <div class="LargeIndexCardsDiv">
         <LargeIndexCard 
-            CardHeading="Master data" 
-            CardText="Find master data for all available SCANGRIP products - in a single file" 
-            CardImg="ikoner/infosite-salestools-ikon.svg"
-            CardDownload=""
+            CardHeading="Product data and prices" 
+            CardText="How to find product information, technical details and prices." 
+            CardImg="public/ikoner/infosite-productdataandprices-ikon.svg"
+            CardDownload="https://partner.scangrip.com/Files/Images/00-PARTNER/PDF/Guides/How%20to%20find%20product%20data%20and%20prices.pdf"
         />
         <LargeIndexCard
-            CardHeading="Media file" 
-            CardText="Download the complete file 'Media' containing pictures, manuals, energy labels, MSDS and DoC info." 
-            CardImg="/ikoner/index-mediafile-ikon.svg"
-            CardDownload="https://partner.scangrip.com/Files/Images/00-Website-pictures/Partner/Marketing%20items/Price%20and%20data/Pictures_Manuals_EnergyLabels_MSDS_DOC_ProductSheet.zip"
+            CardHeading="Marketing materials" 
+            CardText="How to download pictures, videos, brochures, symbols and logos." 
+            CardImg="public/ikoner/infosite-marketingmaterials-ikon.svg"
+            CardDownload="https://partner.scangrip.com/Files/Images/00-PARTNER/PDF/Guides/How%20to%20download%20pictures.pdf"
         />
         <LargeIndexCard 
-            CardHeading="Pricelist" 
-            CardText="Download SCANGRIP's price lists for all products/markets." 
-            CardImg="/ikoner/index-pricelist-ikon.svg"
-            CardDownload="/download/price-list.xlsx"
+            CardHeading="Sales tools" 
+            CardText="How to find sales campaigns, exhibition equipment and other sales tools." 
+            CardImg="public/ikoner/infosite-salestools-ikon.svg"
+            CardDownload="https://partner.scangrip.com/Files/Images/00-PARTNER/PDF/Guides/How%20to%20find%20sales%20tools.pdf"
+        />
+         <LargeIndexCard 
+            CardHeading="Certificates and documentation" 
+            CardText="How to find CE certificates, manuals, energy labels and other documentation." 
+            CardImg="public/ikoner/infosite-certificatesanddocumentation-ikon.svg"
+            CardDownload="https://partner.scangrip.com/Files/Images/00-PARTNER/PDF/Guides/How%20to%20find%20certificates%20and%20documentation.pdf"
         />
     </div>
     <Footer/>
