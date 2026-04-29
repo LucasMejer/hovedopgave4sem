d9391452e76dbe921bc088c33aeb115bb51dd945
Vincent Wiederhold <vincent.taliesin@gmail.com>
2026-04-29 08:31:36 +0200

    slettet unødig kildehenvisnings ord

--- src/views/infoView.vue
+++ src/views/infoView.vue
@@ -10,25 +10,25 @@ import FAQAccordion from '@/components/FAQAccordion.vue';
         <LargeIndexCard 
             CardHeading="Product data and prices" 
             CardText="How to find product information, technical details and prices." 
-            CardImg="public/ikoner/infosite-productdataandprices-ikon.svg"
+            CardImg="/ikoner/infosite-productdataandprices-ikon.svg"
             CardDownload="https://partner.scangrip.com/Files/Images/00-PARTNER/PDF/Guides/How%20to%20find%20product%20data%20and%20prices.pdf"
         />
         <LargeIndexCard
             CardHeading="Marketing materials" 
             CardText="How to download pictures, videos, brochures, symbols and logos." 
-            CardImg="public/ikoner/infosite-marketingmaterials-ikon.svg"
+            CardImg="/ikoner/infosite-marketingmaterials-ikon.svg"
             CardDownload="https://partner.scangrip.com/Files/Images/00-PARTNER/PDF/Guides/How%20to%20download%20pictures.pdf"
         />
         <LargeIndexCard 
             CardHeading="Sales tools" 
             CardText="How to find sales campaigns, exhibition equipment and other sales tools." 
-            CardImg="public/ikoner/infosite-salestools-ikon.svg"
+            CardImg="/ikoner/infosite-salestools-ikon.svg"
             CardDownload="https://partner.scangrip.com/Files/Images/00-PARTNER/PDF/Guides/How%20to%20find%20sales%20tools.pdf"
         />
          <LargeIndexCard 
             CardHeading="Certificates and documentation" 
             CardText="How to find CE certificates, manuals, energy labels and other documentation." 
-            CardImg="public/ikoner/infosite-certificatesanddocumentation-ikon.svg"
+            CardImg="/ikoner/infosite-certificatesanddocumentation-ikon.svg"
             CardDownload="https://partner.scangrip.com/Files/Images/00-PARTNER/PDF/Guides/How%20to%20find%20certificates%20and%20documentation.pdf"
         />
     </div>
