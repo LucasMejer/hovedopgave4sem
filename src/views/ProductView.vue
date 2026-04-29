cbf438be51dfc77d268fc7e7855a170390803d9b
Vincent Wiederhold <vincent.taliesin@gmail.com>
2026-04-29 20:32:10 +0200

    produktside

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -4,7 +4,25 @@ import Navbar from '@/components/Navbar.vue';
 </script>
 <template>
     <Navbar/>
+    <div class="breadcrumb">
+        <li>
+            <router-link to="/">
+                <p>Forside</p>
+            </router-link>
+            <router-link to="/category">
+                <p>Worklights</p>
+            </router-link>
+        </li>
+    </div>
+    <h3>NOVA MINI</h3>
+    <p>03.6200</p>
+    <img src="/novamini.png" alt="">
     <Footer/>
 </template>
 <style lang="scss" scoped>
+img{
+    width: 90%;
+    display: flex;
+    margin: auto;
+}
 </style>
