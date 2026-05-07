89a4a7ff94e1c4155fe37cbd54ab0637ba55313b
SebastianSalling <sebastian-benjamin-s@hotmail.com>
2026-05-07 13:21:00 +0200

    Update searches styling

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,5 +1,10 @@
 <script setup>
+import { ref } from 'vue';
 
+
+function clickInside() {
+
+};
 
 
 /*
@@ -15,22 +20,22 @@ Vis kun det der matcher med det søgte
 
     <div class="search-bar">
         <div class="search-input-container">
-            <input class="search-input" type="text" placeholder="Search...">
+            <input class="search-input" type="text" placeholder="Search..." v-model="search">
             <img class="search-icon" src="../../public/ikoner/search.png" alt="">
         </div>
         
-        <div class="searched-content-container">
-            <div class="searched-content">
+        <div class="searched-dropdown-container">
+            <div class="searched-dropdown">
                 <div class="searched-product">
                     <img class="product-image" src="../../public/novamini.png" alt="">
                     <div class="product-content">
                         <div class="product-text">
                             <h3 class="text">NOVA MINI</h3>
-                            <p class="text">03.6200</p>
+                            <p class="text number">03.6200</p>
                         </div>
                         <div class="product-tags">
-                            <p>Construction</p>
-                            <p>Electrician</p>
+                            <p class="tag">Construction</p>
+                            <p class="tag">Electrician</p>
                         </div>
                     </div>
                 </div>
@@ -49,10 +54,18 @@ Vis kun det der matcher med det søgte
         padding: 15px 0 25px 0;
         width: 90%;
 
+        &:hover .searched-dropdown-container {
+            display: block;
+        }
+
+
         .search-input-container {
             display: flex;
             align-items: center;
             justify-content: flex-end;
+
+
+            
 
             .search-input {
                 height: 30px;
@@ -62,6 +75,8 @@ Vis kun det der matcher med det søgte
                 padding: 5px 10px;
                 font-family: f.$font-primary;
                 font-size: 13px;
+
+                
             }
 
             ::placeholder {
@@ -76,26 +91,28 @@ Vis kun det der matcher med det søgte
             }
         }
 
-        .searched-content-container {
+        .searched-dropdown-container {
+            display: none;
             position: relative;
             z-index: 6;
 
 
-            .searched-content {
-                position: absolute;
+            .searched-dropdown {
+                position: relative;
                 background-color: #fff;
-                box-shadow: 0px 2px 5px 0px rgba(0,0,0,0.4);
+                border: 1px solid #afafaf;
                 list-style-type: none;
                 width: 100%;
-                padding: 10px;
                 box-sizing: border-box;
 
 
                 .searched-product {
                     display: flex;
+                    padding: 10px;
 
                     .product-image {
                         width: 50px;
+                        height: 50px;
                     }
 
                     .product-content {
@@ -108,16 +125,28 @@ Vis kun det der matcher med det søgte
                         .product-text {
                             display: flex;
                             align-items: flex-end;
-                            height: auto;
+                            
+
 
                             .text {
                                 
                                 
                             }
+
+                            .number {
+                                color: c.$hover-state-active;
+                                margin-bottom: 3px;
+                                margin-left: 5px;
+                            }
                         }
 
                         .product-tags {
                             display: flex;
+
+                            .tag {
+                                margin-right: 5px;
+                            }
+                            
                         }
                     }
 
@@ -126,6 +155,10 @@ Vis kun det der matcher med det søgte
 
                 
             }
+        }
+
+        .hide-searches {
+            display: none;
         }
 
     }
@@ -147,20 +180,27 @@ Vis kun det der matcher med det søgte
                 }
 
 
-                .searched-content-container {
+                .searched-dropdown-container {
                     
 
 
-                    .searched-content {
-                        /*
+                    .searched-dropdown {
+                        
                         position: absolute;
-                        background-color: #fff;
                         box-shadow: 0px 2px 5px 0px rgba(0,0,0,0.4);
+                        border: none;
+                        /*
                         list-style-type: none;
                         width: 100%;
-                        padding: 10px;
                         box-sizing: border-box;*/
 
+                        .searched-product {
+
+                            &:hover {
+                                background-color: #e6e6e6;
+                                cursor: pointer;
+                            }
+                        }
 
 
 
